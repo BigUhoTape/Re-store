51d0cb6b0554b965c1ae60f35f3e3c6c97bdd9f4
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-06-01 15:19:18 +0700

    basic components

---
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './ErrorIndicator.css';
+
+const ErrorIndicator = () => {
+  return (
+    <div>Error Indicator Component</div>
+  );
+};
+
+
+export default ErrorIndicator;
---
+++ src/components/HOC/WithBookstoreService.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BookstoreServiceConsumer } from './../BookstoreServiceContext/BookstoreServiceContext';
+
+const WithBookstoreService = () => (Wrapped) => {
+
+  return (props) => {
+    return (
+      <BookstoreServiceConsumer>
+        {
+          (bookStoreService) => {
+            return <Wrapped {...props} bookStoreService={bookStoreService}/>
+          }
+        }
+      </BookstoreServiceConsumer>
+    );
+  }
+};
+
+export default WithBookstoreService;
