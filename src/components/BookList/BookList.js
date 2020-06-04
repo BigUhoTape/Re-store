d2f42aff23e204b9097e4ec50acbb90e1f23ecba
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-06-04 18:20:08 +0700

    async data

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -5,18 +5,25 @@ import { bookLoaded } from './../../actions/index';
 import './BookList.css';
 
 import BookListItem from "../BookListItem/BookListItem";
+import Spinner from "../Spinner/Spinner";
 
 class BookList extends Component{
 
   componentDidMount() {
     const { bookStoreService } = this.props;
-    const data = bookStoreService.getBooks();
-
-    this.props.bookLoaded(data);
+    bookStoreService.getBooks()
+      .then(data => {
+        this.props.bookLoaded(data);
+      })
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+
+    if (loading) {
+      return <Spinner/>
+    }
+
     return (
       <ul className="book-list">
         {
@@ -33,8 +40,8 @@ class BookList extends Component{
   }
 }
 
-const mapStateToProps = ({ books }) => {
-  return { books }
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading }
 };
 
 const mapDispatchToProps = {
