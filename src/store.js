5e6ee990cf9eb7651df2fd66afeb1cb23cfcf414
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-06-07 15:33:53 +0700

    fix

--- src/store.js
+++ src/store.js
@@ -4,12 +4,4 @@ import reducer from "./reducers";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
-const myAction = (dispatch) => {
-  setTimeout(() => dispatch({
-    type: 'DELAY_ACTION'
-  }), 2000)
-};
-
-store.dispatch(myAction);
-
 export default store;
