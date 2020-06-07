680baadb5ad3edcda3ef7e9d7b81dfe357c2a939
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-06-07 15:13:10 +0700

    thunk

--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const myAction = (dispatch) => {
+  setTimeout(() => dispatch({
+    type: 'DELAY_ACTION'
+  }), 2000)
+};
+
+store.dispatch(myAction);
 
 export default store;
