e287c5d4c0a2141c2558032544d3807a7e29a732
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-12-16 13:03:13 +0700

    7

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import ShopHeader from "../ShopHeader/ShopHeader";
 import './App.css'
 
-import CartPage from "../Pages/CartPage";
 import HomePage from "../Pages/HomePage";
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
       <ShopHeader numItems={5} total={210}/>
       <Switch>
         <Route path="/Re-store/"  component={ HomePage }/>
-        <Route path="/Re-store/cart"  component={ CartPage }/>
       </Switch>
     </main>
   );
